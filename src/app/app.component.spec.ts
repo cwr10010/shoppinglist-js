9bd3aa82e2405d1277ef847ea93cb199fae07128
cwr10010 <github@cwrose.de>
2017-09-03 19:58:30 +0200

    make tests work again. nothing special here for now

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,58 @@
 import { TestBed } from '@angular/core/testing';
 
+import {APP_BASE_HREF} from '@angular/common';
+
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { CookieService } from 'ngx-cookie-service';
+
+import { HttpClient } from './services/http-client.module';
+
+import { ShoppingListService } from './services/shoppinglist.service';
+import { ShoppingListItemSearchService } from './services/item-search.service';
+import { LocalStorageService } from './services/local-storage.service';
+import { AuthorizationService } from './services/authorization.service';
+import { LoggingService } from './services/logging.service';
+
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ShoppingListComponent } from './shoppinglist/shoppinglist.component';
+import { ShoppingListItemSearchComponent } from './item-search/item-search.component';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 describe('App', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
+    TestBed.configureTestingModule({
+        imports: [
+            BrowserModule,
+            FormsModule,
+            HttpModule,
+            AppRoutingModule
+        ],
+        declarations: [
+            AppComponent,
+            LoginComponent,
+            DashboardComponent,
+            ItemDetailsComponent,
+            ShoppingListComponent,
+            ShoppingListItemSearchComponent
+        ],
+        providers: [
+            ShoppingListService,
+            ShoppingListItemSearchService,
+            CookieService,
+            LocalStorageService,
+            AuthorizationService,
+            LoggingService,
+            HttpClient,
+            {provide: APP_BASE_HREF, useValue : '/' }
+        ]
+    });
   });
 
   it ('should work', () => {
