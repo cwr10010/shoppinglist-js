84a715a4463752b9f1aec31686c82bcdae0f296d
cwr10010 <github@cwrose.de>
2017-09-15 21:36:24 +0200

    minimize test setup for application spec. start implementing tests here now.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,60 +1,65 @@
-import { TestBed } from '@angular/core/testing';
-
-import {APP_BASE_HREF} from '@angular/common';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-import { CookieService } from 'ngx-cookie-service';
+import { AppRoutingModule } from './app-routing.module';
+import { CustomMaterialModule } from './custom-material.module';
 
-import { HttpClient } from './_helpers/http-client';
-import { Logger } from './_helpers/logging';
-
-import { ShoppingListService } from './_services/shoppinglist.service';
-import { ShoppingListItemSearchService } from './_services/item-search.service';
-import { LocalStorageService } from './_services/local-storage.service';
-import { AuthorizationService } from './_services/authorization.service';
-
+import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ItemDetailsComponent } from './item-details/item-details.component';
 import { ShoppingListItemSearchComponent } from './item-search/item-search.component';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { Logger } from './_helpers/logging';
+import { AuthorizationService } from './_services/authorization.service';
+
+class RouterMock {
+  navigate(url: string[]) { return url; }
+}
+
+class AuthorizationServiceMock {
+  getAuthToken() { return 'fake-auth-token'; }
+}
 
 describe('App', () => {
-  beforeEach(() => {
+  let appComponent: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let debugElement: DebugElement;
+
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
         imports: [
             BrowserModule,
             FormsModule,
-            HttpModule,
-            AppRoutingModule
+            AppRoutingModule,
+            CustomMaterialModule
         ],
         declarations: [
-            AppComponent,
             LoginComponent,
             DashboardComponent,
+            ShoppingListItemSearchComponent,
             ItemDetailsComponent,
-            ShoppingListItemSearchComponent
+            AppComponent
         ],
         providers: [
-            ShoppingListService,
-            ShoppingListItemSearchService,
-            CookieService,
-            LocalStorageService,
-            AuthorizationService,
             Logger,
-            HttpClient,
-            {provide: APP_BASE_HREF, useValue : '/' }
+            { provide: AuthorizationService, useClass: AuthorizationServiceMock },
+            { provide: Router, useClass: RouterMock }
         ]
-    });
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    appComponent = fixture.componentInstance;
+
   });
 
   it ('should work', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+    expect(appComponent instanceof AppComponent).toBe(true, 'should create AppComponent');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { AuthorizationService } from './_services/authorization.service';
 
