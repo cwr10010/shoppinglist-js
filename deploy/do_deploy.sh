8f8456d0e1b8ae682b28e786412d1184ccf34260
cwr10010 <github@cwrose.de>
2017-09-26 20:19:24 +0200

    add checkbox for checked into detailed view and implement deployment to server

---
+++ deploy/do_deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/env sh
+
+if [ -z "$TRAVIS_BUILD_DIR" ]; then
+    export TRAVIS_BUILD_DIR="../"
+fi
+
+if [ -z "$1" ]; then
+    export PROFILE=dev
+else
+    export PROFILE=$1
+fi
+
+TARGET_USER=cwrosede
+TARGET_HOST=cwrose.de
+TARGET_DIR=subdomains/shoppinglist/
+TARGET=$TARGET_USER@$TARGET_HOST:$TARGET_DIR
+
+ssh $TARGET_USER@$TARGET_HOST "hostname; cd $TARGET_DIR/shoppinglist-config && git pull"
+ssh $TARGET_USER@$TARGET_HOST docker-compose  -f $TARGET_DIR/shoppinglist-config/compose/docker-compose-$PROFILE.yml stop srs_js_${PROFILE}
+ssh $TARGET_USER@$TARGET_HOST docker-compose  -f $TARGET_DIR/shoppinglist-config/compose/docker-compose-$PROFILE.yml pull srs_js_${PROFILE}
+ssh $TARGET_USER@$TARGET_HOST docker-compose  -f $TARGET_DIR/shoppinglist-config/compose/docker-compose-$PROFILE.yml up -d srs_js_${PROFILE}
